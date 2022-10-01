a1c0a092d019b24958e73c7ada81a0a747fbc7e4
TIANYUZHOU <zty.zhou@foxmail.com>
2022-10-01 13:40:54 +0800

    Add several methods

--- autoqa.py
+++ autoqa.py
@@ -2,45 +2,52 @@
 QA
 """
 
-import time
-from typing import List, Optional
+# pylint: disable=E,W,C
+
+import random
+import configparser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-UESER_INFO_ROOT = "./userinfo.json"
-QA_URL_ROOT = "./qaurl.txt"
-
-URLS = [""]
-
+from utils.text_rewriting import rewrite
 
 class AutoQA:
     r"""
     Automatic question and answer
     """
 
-    def __init__(self, urls: Optional[List[str]] = None):
+    def __init__(self):
         self.user_info = {}
         self.urls = []
-        self.read_urls(urls)
+        self.get_config()
         self.driver = webdriver.Chrome()
 
-    def read_urls(self, urls: Optional[List[str]] = None):
+    def get_config(self):
         r"""
-        read urls
+        get config
         """
-        if urls is None:
-            with open(QA_URL_ROOT, 'r', encoding="utf8") as url_data:
-                for line in url_data.readlines():
-                    self.urls.append(line.strip())
-        else:
-            self.urls.extend(urls)
-        return self.urls
+        file = 'config/config.ini'
+        config = configparser.ConfigParser()
+        config.read(file, encoding="utf-8")
+        self.user_info = eval(config.get('USER_INFO', 'USER_INFO'))
+        self.urls = eval(config.get('URL', 'URLS'))
+
+    def get_user_info(self):
+        r"""
+        get user info
+        """
+        return self.user_info
 
     def get_urls(self):
         r"""
         get urls
         """
         return self.urls
+
+    def get_driver(self):
+        r"""
+        get driver
+        """
+        return self.driver
 
     def login_qapage(self):
         r"""
@@ -56,24 +63,86 @@ class AutoQA:
         input_username.send_keys(self.user_info['username'])
         input_passwordd.send_keys(self.user_info['password'])
         login_botton.click()
-
-    def get_qa(self):
+        
+    def select_question(self):
         r"""
-        get qa
+        select question
         """
         question_xpath = '//*[@id="app"]/div/div[2]/div[1]\
             /div/div[2]/div[1]/div/ul/li[@class="question-item"]'
-        question = self.driver.find_element(By.XPATH, question_xpath)
-        question.click()
-        answer_xpath = '//*[@id="app"]/div/div[3]/div[1]/div[2]\
-            /ul/li[1]/div[@class="answer-content"]/p/span'
-        answer = self.driver.find_element(By.XPATH, answer_xpath).text
-        print(answer)
+        # find_elements 返回的是列表，并且如果没匹配到，返回空列表
+        question = self.driver.find_elements(By.XPATH, question_xpath)
+        question[1].click()     
+        
+    def get_answer(self): 
+        windows = self.driver.window_handles
+        self.driver.switch_to.window(windows[-1])
+
+        is_answered_xpath = '//*[@id="app"]/div/div[4]/span'
+        is_answered = self.driver.find_elements(By.XPATH, is_answered_xpath)
+        if len(is_answered) != 0:
+            answers_list_xpath = '//*[@id="app"]/div/div[3]/div[1]/div[2]/ul/li'
+            # 问题下面的回答列表
+            answers_list = self.driver.find_elements(By.XPATH, answers_list_xpath)
+            # 如果长度为 0 说明这个问题还没人回答，则关掉该问题标签页，并切换到前一页
+            if len(answers_list) == 0:
+                self.driver.close()
+                self.driver.switch_to.window(windows[0])
+                return False
+            else:
+                answer_text_list = []
+                for answer in answers_list:
+                    answer_text = answer.find_elements(By.XPATH, 'div[@class="answer-content"]/p/span')[0].text
+                    answer_text_list.append(answer_text)
+                
+                final_answer_text_list = []
+                min_len = 31
+                
+                while len(final_answer_text_list) == 0:
+                    for answer in answer_text_list:
+                        if len(answer) > min_len:
+                            final_answer_text_list.append(answer)
+                    min_len -= 5
+
+                print(len(final_answer_text_list))
+                # print(final_answer_text_list)
+                random.shuffle(final_answer_text_list)
+                # print(final_answer_text_list)
+                is_answered[0].click()
+                return final_answer_text_list
+        else:
+            print("已经回答过此问题！")
+            self.driver.close()
+            self.driver.switch_to.window(windows[0])
+            return False
+
+    def input_answer(self, text_list: list):
+        r"""
+        input answer
+        """
+        inputbox_xpath = '//*[@id="app"]/div/div\
+            [@class="questionDialog ZHIHUISHU_QZMD"]/div/div/div[2]/div[1]/div[1]/div/textarea'
+        inputbox = self.driver.find_elements(By.XPATH, inputbox_xpath)
+        inputbox[0].send_keys(text_list[0])
+
+    def submmit_answer(self):
+        r"""
+        submmit answer
+        """
+        submit_botton_xpath = '//*[@id="app"]/div/div\
+            [@class="questionDialog ZHIHUISHU_QZMD"]/div/div/div[2]/div[1]/div[2]/div'
+
+        submit_botton = self.driver.find_elements(By.XPATH, submit_botton_xpath)
+
+        # submit_botton[0].click()
 
 
 if __name__ == '__main__':
-    AUTO_QA = AutoQA()
+    # AUTO_QA = AutoQA()
+    # print(AUTO_QA.get_urls()[0])
+    # print(type(AUTO_QA.get_urls()[0]))
+    # AUTO_QA.login_qapage()
+    # time.sleep(5)
+    # AUTO_QA.get_qa()
     # print(AUTO_QA.get_urls())
-    AUTO_QA.login_qapage()
-    time.sleep(5)
-    AUTO_QA.get_qa()
+    pass
---
+++ utils/text_rewriting.py
@@ -0,0 +1,148 @@
+'''
+1、文本改写 Web API 调用示例
+2、运行前：请先填写Appid、APIKey、APISecret 相关信息
+'''
+from datetime import datetime
+from wsgiref.handlers import format_date_time
+from time import mktime
+import hashlib
+import base64
+import hmac
+from urllib.parse import urlencode
+import json
+import configparser
+import requests
+
+# pylint: disable=E,W,C
+
+
+
+class AssembleHeaderException(Exception):
+    def __init__(self, msg):
+        self.message = msg
+
+
+class Url:
+    def __init__(self, host, path, schema):
+        self.host = host
+        self.path = path
+        self.schema = schema
+
+
+class wsParam(object):
+    def __init__(self):
+        self.APPID = ""
+        self.APIKey = ""
+        self.APISecret = ""
+        self.url = 'https://api.xf-yun.com/v1/private/se3acbe7f'
+        self.level = ""
+        self.get_config()
+
+    def parse_url(self, requset_url):
+        stidx = requset_url.index("://")
+        host = requset_url[stidx + 3:]
+        schema = requset_url[:stidx + 3]
+        edidx = host.index("/")
+        if edidx <= 0:
+            raise AssembleHeaderException("invalid request url:" + requset_url)
+        path = host[edidx:]
+        host = host[:edidx]
+        u = Url(host, path, schema)
+        return u
+
+    def init_header(self):
+        headers = {
+            'content-type': "application/json",
+            'host': 'api.xf-yun.com'
+        }
+        return headers
+
+    def get_body(self, text):
+        data = {
+            "header": {
+                "app_id": self.APPID,
+                "status": 3,
+            },
+            "parameter": {
+                "se3acbe7f": {
+                    "level": self.level,
+                    "result": {
+                        "encoding": "utf8",
+                        "compress": "raw",
+                        "format": "json"
+                    }
+                }
+            },
+            "payload": {
+                "input1": {
+                    "encoding": "utf8",
+                    "compress": "raw",
+                    "format": "plain",
+                    "status": 3,
+                    "text": str(base64.b64encode(text.encode('utf-8')), 'utf-8')
+                }
+            }
+        }
+        body = json.dumps(data)
+        return body
+    
+    def get_config(self):
+        file = 'config/config.ini'
+        config = configparser.ConfigParser()
+        config.read(file, encoding="utf-8")
+        self.APPID = config.get('API', 'APPID')
+        self.APIKey = config.get('API', 'APIKey')
+        self.APISecret = config.get('API','APISecret')
+        self.level = "<L5>"  # 改写等级 <L1>  ~  <L6>  等级越高，改写程度越深
+
+
+def assemble_ws_auth_url(requset_url, method="POST", api_key="", api_secret=""):
+    u = wsParam.parse_url(requset_url)
+    host = u.host
+    path = u.path
+    now = datetime.now()
+    date = format_date_time(mktime(now.timetuple()))
+    # print(date)
+    # date = "Thu, 12 Dec 2019 01:57:27 GMT"
+    signature_origin = "host: {}\ndate: {}\n{} {} HTTP/1.1".format(
+        host, date, method, path)
+    # print("----2", signature_origin)
+    signature_sha = hmac.new(api_secret.encode('utf-8'), signature_origin.encode('utf-8'),
+                             digestmod=hashlib.sha256).digest()
+    signature_sha = base64.b64encode(signature_sha).decode(encoding='utf-8')
+    authorization_origin = "api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"" % (
+        api_key, "hmac-sha256", "host date request-line", signature_sha)
+    # print("----1:", authorization_origin)
+    authorization = base64.b64encode(
+        authorization_origin.encode('utf-8')).decode(encoding='utf-8')
+    # print(authorization_origin)
+    values = {
+        "host": host,
+        "date": date,
+        "authorization": authorization
+    }
+    return requset_url + "?" + urlencode(values)
+
+
+def rewrite(text: str):
+    global wsParam
+    wsParam = wsParam()
+    request_url = assemble_ws_auth_url(
+        wsParam.url, "POST", wsParam.APIKey, wsParam.APISecret)
+    # print("request_url:", request_url)
+    response = requests.post(
+        request_url, data=wsParam.get_body(text), headers=wsParam.init_header())
+    # print("response:", response)
+    str_result = response.content.decode('utf8')
+    json_result = json.loads(str_result)
+    # print("response-content:", json_result)
+    if json_result. __contains__('header') and json_result['header']['code'] == 0:
+        renew_text_list = json_result['payload']['result']['text']
+        renew_text = eval(str(base64.b64decode(renew_text_list), 'utf-8'))
+        print("\n改写结果：", str(base64.b64decode(renew_text_list), 'utf-8'))
+        print(renew_text[0][0])
+
+
+# if __name__ == "__main__":
+#     rewrite("")
+    
